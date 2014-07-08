5053fd8f562a3c7d80df82a8a25bf8b2b4c96ac5
lukebranch <lukebranch@gmail.com>
2014-07-08 16:39:15 +0800

    curl -L -O to follow mirror redirection from downloads.sourceforge.net

--- odoo-saas4/ubuntu-14-04/odoo_install.sh
+++ odoo-saas4/ubuntu-14-04/odoo_install.sh
@@ -86,7 +86,8 @@ sudo pip install graphviz ghostscript gcc mc bzr lptools make
 sudo pip install gevent gevent_psycopg2 psycogreen
 
 echo -e "\n---- Install Wkhtmltopdf 0.12.1 ----"
-sudo wget http://kaz.dl.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
+# curl -L to follow mirror redirect from sourceforge.net (eg. kaz.sourceforge.net...)
+sudo curl -L -O http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
 sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
