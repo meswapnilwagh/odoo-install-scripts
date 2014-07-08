fe43f61995a890532ba9693880d1c2864fa42691
lukebranch <lukebranch@gmail.com>
2014-07-08 18:49:50 +0800

    postgresql-9.3

--- odoo-saas4/ubuntu-14-04/odoo_install.sh
+++ odoo-saas4/ubuntu-14-04/odoo_install.sh
@@ -60,7 +60,7 @@ sudo apt-get install ssh -y
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt-get install postgresql-9.3 -y
+sudo apt-get install postgresql-9.3 -y 
 
 	
 echo -e "\n---- PostgreSQL $PG_VERSION Settings  ----"
