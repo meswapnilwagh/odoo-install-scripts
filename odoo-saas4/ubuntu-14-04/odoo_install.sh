47977061b7aab8e05fb514bfc6445c13f9b309b0
lukebranch <lukebranch@gmail.com>
2014-10-17 10:39:10 +0800

    add sudo pip install --pre pyusb
    add sudo pip install --pre pyusb for pyusb 1.0+ (not stable) for compatibility with hw_escpos for receipt printer and cash drawer module install. 
    
    To solve the following error:
    
    Unable to install module "hw_escpos" because an external dependency is not met: No module named usb.core

--- odoo-saas4/ubuntu-14-04/odoo_install.sh
+++ odoo-saas4/ubuntu-14-04/odoo_install.sh
@@ -139,9 +139,12 @@ echo -e "\n---- Install and Upgrade pip and virtualenv ----"
 sudo apt-get install python-dev build-essential -y
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
+echo -e "\n---- Install pyserial and qrcode for compatibility with hw_ modules for peripheral support in Odoo ---"
 sudo pip install pyserial
 sudo pip install qrcode
-sudo pip install pyusb
+
+echo -e "\n---- Install pyusb 1.0+ not stable for compatibility with hw_escpos for receipt printer and cash drawer support in Odoo ---"
+sudo pip install --pre pyusb
 	
 echo -e "\n---- Install python packages ----"
 sudo apt-get install -y --force-yes --no-install-recommends python-gevent python-dateutil python-feedparser python-gdata python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-pdftools python-setuptools python-pybabel python-imaging python-matplotlib python-reportlab-accel python-openssl python-egenix-mxdatetime python-paramiko antiword python-decorator poppler-utils python-requests libpq-dev python-geoip python-markupsafe python-uno postgresql-client python-passlib vim libreoffice curl openssh-server npm python-cairo python-genshi libreoffice-script-provider-python
