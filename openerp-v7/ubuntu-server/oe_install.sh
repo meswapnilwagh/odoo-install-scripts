b973d2e7e78d5f0c8f957050b82fcb47cd593eed
Swapnil Wagh <Swapnil.wagh@pragtech.co.in>
2015-11-04 12:46:01 +0530

    chnage the wkhtmltopdf package download url

--- openerp-v7/ubuntu-server/oe_install.sh
+++ openerp-v7/ubuntu-server/oe_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ################################################################################
-# Script for Installation: ODOO V8 server on Ubuntu Server 
+# Script for Installation: ODOO V8 server on Ubuntu Server
 # Author: André Schenkels, ICTSTUDIO 2014
 # Forked & Modified by: Swapnil A. Wagh
 #-------------------------------------------------------------------------------
-#  
+#
 # This script will install ODOO V7 on
 # clean Ubuntu Server
 #-------------------------------------------------------------------------------
@@ -13,10 +13,10 @@
 # odoo-install
 #
 # EXAMPLE:
-# sudo ./odoo-install 
+# sudo ./odoo-install
 #
 ################################################################################
- 
+
 
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
@@ -43,7 +43,7 @@ OE_CONFIG="$OE_USER-server"
 echo -e "\n---- Update Server ----"
 sudo apt-get upgrade -y
 sudo apt-get update -y
-	
+
 #--------------------------------------------------
 # Set Locale en_US.UTF-8
 #--------------------------------------------------
@@ -59,7 +59,7 @@ sudo su root -c "echo 'LANGUAGE="en_US:en"' >> /etc/default/locale"
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt-get install postgresql -y  
+sudo apt-get install postgresql -y
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
@@ -79,7 +79,7 @@ sudo apt-get -f install -y
 
 echo -e "\n---- Install pyusb 1.0+ not stable for compatibility with hw_escpos for receipt printer and cash drawer support in Odoo ---"
 sudo pip install --pre pyusb
-	
+
 echo -e "\n---- Install python packages ----"
 sudo apt-get install -y --force-yes --no-install-recommends python-gevent python-dateutil python-feedparser python-gdata python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-pdftools python-setuptools python-pybabel python-imaging python-matplotlib python-reportlab-accel python-openssl python-egenix-mxdatetime python-paramiko antiword python-decorator poppler-utils python-requests libpq-dev python-geoip python-markupsafe postgresql-client python-passlib vim libreoffice curl openssh-server npm python-cairo python-genshi libreoffice-script-provider-python
 
@@ -92,11 +92,11 @@ sudo pip install graphviz ghostscript gcc mc bzr lptools make
 sudo pip install gevent gevent_psycopg2 psycogreen
 
 echo -e "\n---- Install Wkhtmltopdf 0.12.1 ----"
-sudo wget http://downloads.sourceforge.net/project/wkhtmltopdf/archive/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
+sudo wget http://download.gna.org/wkhtmltopdf/0.12/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo cp /usr/local/bin/wkhtmltopdf /usr/bin
 sudo cp /usr/local/bin/wkhtmltoimage /usr/bin
-	
+
 echo -e "\n---- Create ODOO system user ----"
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
 
